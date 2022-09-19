7172807e35beeb1b406495514e6781ba7cf41d35
Воловей Иван <ivan.volovey@mail.ru>
2022-09-19 18:14:39 +0500

    Написание вспомогательных функций

--- js/main.js
+++ js/main.js
@@ -1,21 +1,17 @@
 const GetRandomNumber = function (lowerLimit, upperLimit) {
   if (lowerLimit >= 0 && upperLimit >= lowerLimit) {
-    console.log(Math.floor(Math.random()*(upperLimit-lowerLimit) + lowerLimit));
-  } else {
-    console.log('Введите положительные значения.');
+    return Math.floor(Math.random()*(upperLimit-lowerLimit) + lowerLimit);
   }
+  return 'Введите положительные значения.';
 };
 
-// Проверка
-GetRandomNumber(10, 120);
+// console.log(GetRandomNumber(10, 120));
 
 const CheckStringLength = function (testString, maxLength) {
   if (testString.length < maxLength) {
-    console.log('Длина строки соответствует критериям.');
-  } else {
-    console.log('Длина строки не соответствует критериям.');
+    return 'Длина строки соответствует критериям.';
   }
+  return 'Длина строки не соответствует критериям.';
 };
 
-// Проверка
-CheckStringLength('Запомни Меня [Каво? Деда]', 120);
+// CheckStringLength('Запомни Меня [Каво? Деда]', 120);
