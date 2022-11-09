3c99e784d1305a80e72927d43827d1b96bd433de
Keks <keks@htmlacademy.ru>
2022-11-09 15:07:01 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/thumbnails.js
@@ -0,0 +1,28 @@
+import {getRandomUserPhotos} from './data.js';
+import {createBigPicture} from './big-picture.js';
+
+const renderThumbnails = function () {
+  const photosTemplate = document.querySelector('#picture').
+    content.
+    querySelector('.picture');
+
+  const photosContainer = document.querySelector('.pictures');
+  const containerFragment = document.createDocumentFragment();
+  const createPhotos = getRandomUserPhotos();
+
+  createPhotos.forEach(({url, likes, comments}) => {
+    const photos = photosTemplate.cloneNode(true);
+    photos.querySelector('.picture__img').src = url;
+    photos.querySelector('.picture__likes').textContent = likes;
+    photos.querySelector('.picture__comments').textContent = comments;
+    containerFragment.appendChild(photos);
+
+    photos.addEventListener('click', () => {
+      createBigPicture({url, likes, comments});
+    });
+  });
+
+  photosContainer.appendChild(containerFragment);
+};
+
+export {renderThumbnails};
